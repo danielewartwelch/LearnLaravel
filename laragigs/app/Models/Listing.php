4616561b788f44bc9bb6b0c2ecb774ae0690030f
Daniel Welch <danielewartwelch@gmail.com>
2023-06-20 16:30:59 -0400

    adding mysql, listings migration, and two seeded listings

--- laragigs/app/Models/Listing.php
+++ laragigs/app/Models/Listing.php
@@ -1,28 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all(){
-          return  [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'description for first listing'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'description for second listing'
-            ]
-            ]; 
-        }
+namespace App\Models;
 
-    public static function find($id){
-        $listings = self::all();
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
-}  
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
--- laragigs/database/seeders/DatabaseSeeder.php
+++ laragigs/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'This is the song that doesn\'t end.
+            Yes, it goes on and on, my friend.
+            Some people started singing it, not knowing what it was.
+            And, they\'ll continue singing'
+        ]);
+        Listing::create([
+            'title' => 'Senior QA',
+            'tags' => 'QA, testing, quality',
+            'company' => 'Bugs R Us',
+            'location' => 'Baltimore, MD',
+            'email' => 'ladybug@bugsrus.com',
+            'website' => 'https://www.bugsrus.com',
+            'description' => 'Snape: Snape, Snape, Severus Snape. Snape, Snape, Severus Snape.
+            Dumbledore: (pops up) Dumbledore!
+            Snape: Snape, Snape, Severus Snape.
+            Dumbledore: (pops up) Dumbledore!'
+        ]);
     }
 }
